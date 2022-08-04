dd0680585ac2d088eac47b8ed7dcf975f1589aa3
ZhenmingAo <97646866+ZhenmingAo@users.noreply.github.com>
2022-08-03 21:03:40 -0400

    Add project files.

---
+++ TicTacToe2022.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TicTacToe2022
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TicTacToe2022))
+        Me.btnBox1 = New System.Windows.Forms.Button()
+        Me.btnBox2 = New System.Windows.Forms.Button()
+        Me.btnBox3 = New System.Windows.Forms.Button()
+        Me.btnBox4 = New System.Windows.Forms.Button()
+        Me.btnBox5 = New System.Windows.Forms.Button()
+        Me.btnBox6 = New System.Windows.Forms.Button()
+        Me.btnBox7 = New System.Windows.Forms.Button()
+        Me.btnBox8 = New System.Windows.Forms.Button()
+        Me.btnBox9 = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnBox1
+        '
+        Me.btnBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox1.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox1.Location = New System.Drawing.Point(12, 12)
+        Me.btnBox1.Name = "btnBox1"
+        Me.btnBox1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox1.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox1.TabIndex = 0
+        Me.btnBox1.UseVisualStyleBackColor = True
+        '
+        'btnBox2
+        '
+        Me.btnBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox2.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox2.Location = New System.Drawing.Point(118, 12)
+        Me.btnBox2.Name = "btnBox2"
+        Me.btnBox2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox2.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox2.TabIndex = 1
+        Me.btnBox2.UseVisualStyleBackColor = True
+        '
+        'btnBox3
+        '
+        Me.btnBox3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox3.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox3.Location = New System.Drawing.Point(224, 12)
+        Me.btnBox3.Name = "btnBox3"
+        Me.btnBox3.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox3.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox3.TabIndex = 2
+        Me.btnBox3.UseVisualStyleBackColor = True
+        '
+        'btnBox4
+        '
+        Me.btnBox4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox4.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox4.Location = New System.Drawing.Point(12, 118)
+        Me.btnBox4.Name = "btnBox4"
+        Me.btnBox4.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox4.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox4.TabIndex = 3
+        Me.btnBox4.UseVisualStyleBackColor = True
+        '
+        'btnBox5
+        '
+        Me.btnBox5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox5.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox5.Location = New System.Drawing.Point(118, 118)
+        Me.btnBox5.Name = "btnBox5"
+        Me.btnBox5.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox5.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox5.TabIndex = 4
+        Me.btnBox5.UseVisualStyleBackColor = True
+        '
+        'btnBox6
+        '
+        Me.btnBox6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox6.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox6.Location = New System.Drawing.Point(224, 118)
+        Me.btnBox6.Name = "btnBox6"
+        Me.btnBox6.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox6.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox6.TabIndex = 5
+        Me.btnBox6.UseVisualStyleBackColor = True
+        '
+        'btnBox7
+        '
+        Me.btnBox7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox7.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox7.Location = New System.Drawing.Point(12, 224)
+        Me.btnBox7.Name = "btnBox7"
+        Me.btnBox7.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox7.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox7.TabIndex = 6
+        Me.btnBox7.UseVisualStyleBackColor = True
+        '
+        'btnBox8
+        '
+        Me.btnBox8.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox8.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox8.Location = New System.Drawing.Point(118, 224)
+        Me.btnBox8.Name = "btnBox8"
+        Me.btnBox8.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox8.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox8.TabIndex = 7
+        Me.btnBox8.UseVisualStyleBackColor = True
+        '
+        'btnBox9
+        '
+        Me.btnBox9.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnBox9.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBox9.Location = New System.Drawing.Point(224, 224)
+        Me.btnBox9.Name = "btnBox9"
+        Me.btnBox9.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnBox9.Size = New System.Drawing.Size(100, 100)
+        Me.btnBox9.TabIndex = 8
+        Me.btnBox9.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnReset.Location = New System.Drawing.Point(118, 335)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(100, 30)
+        Me.btnReset.TabIndex = 9
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'TicTacToe2022
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 377)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnBox9)
+        Me.Controls.Add(Me.btnBox8)
+        Me.Controls.Add(Me.btnBox7)
+        Me.Controls.Add(Me.btnBox6)
+        Me.Controls.Add(Me.btnBox5)
+        Me.Controls.Add(Me.btnBox4)
+        Me.Controls.Add(Me.btnBox3)
+        Me.Controls.Add(Me.btnBox2)
+        Me.Controls.Add(Me.btnBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "TicTacToe2022"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TicTacToe2022"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnBox1 As Button
+    Friend WithEvents btnBox2 As Button
+    Friend WithEvents btnBox3 As Button
+    Friend WithEvents btnBox4 As Button
+    Friend WithEvents btnBox5 As Button
+    Friend WithEvents btnBox6 As Button
+    Friend WithEvents btnBox7 As Button
+    Friend WithEvents btnBox8 As Button
+    Friend WithEvents btnBox9 As Button
+    Friend WithEvents btnReset As Button
+End Class
---
+++ TicTacToe2022.vb
@@ -0,0 +1,215 @@
+﻿' Fun TicTacToe project, updated as 2022 version
+' Author: Zhenming Ao
+Public Class TicTacToe2022
+
+    Dim blnXO As Boolean = True 'Default starts off with "X"
+
+    Private Sub btnBox1_Click(sender As Object, e As EventArgs) Handles btnBox1.Click
+        If blnXO = True Then
+            btnBox1.Text = "X"
+            blnXO = False
+        Else
+            btnBox1.Text = "O"
+            blnXO = True
+        End If
+        btnBox1.Enabled = False
+        checkForWinner()
+    End Sub
+
+    Private Sub btnBox2_Click(sender As Object, e As EventArgs) Handles btnBox2.Click
+        If blnXO = True Then
+            btnBox2.Text = "X"
+            blnXO = False
+        Else
+            btnBox2.Text = "O"
+            blnXO = True
+        End If
+        btnBox2.Enabled = False
+        checkForWinner()
+    End Sub
+    Private Sub btnBox3_Click(sender As Object, e As EventArgs) Handles btnBox3.Click
+        If blnXO = True Then
+            btnBox3.Text = "X"
+            blnXO = False
+        Else
+            btnBox3.Text = "O"
+            blnXO = True
+        End If
+        btnBox3.Enabled = False
+        checkForWinner()
+    End Sub
+
+    Private Sub btnBox4_Click(sender As Object, e As EventArgs) Handles btnBox4.Click
+        If blnXO = True Then
+            btnBox4.Text = "X"
+            blnXO = False
+        Else
+            btnBox4.Text = "O"
+            blnXO = True
+        End If
+        btnBox4.Enabled = False
+        checkForWinner()
+    End Sub
+
+    Private Sub btnBox5_Click(sender As Object, e As EventArgs) Handles btnBox5.Click
+        If blnXO = True Then
+            btnBox5.Text = "X"
+            blnXO = False
+        Else
+            btnBox5.Text = "O"
+            blnXO = True
+        End If
+        btnBox5.Enabled = False
+        checkForWinner()
+    End Sub
+
+    Private Sub btnBox6_Click(sender As Object, e As EventArgs) Handles btnBox6.Click
+        If blnXO = True Then
+            btnBox6.Text = "X"
+            blnXO = False
+        Else
+            btnBox6.Text = "O"
+            blnXO = True
+        End If
+        btnBox6.Enabled = False
+        checkForWinner()
+    End Sub
+
+    Private Sub btnBox7_Click(sender As Object, e As EventArgs) Handles btnBox7.Click
+        If blnXO = True Then
+            btnBox7.Text = "X"
+            blnXO = False
+        Else
+            btnBox7.Text = "O"
+            blnXO = True
+        End If
+        btnBox7.Enabled = False
+        checkForWinner()
+    End Sub
+
+    Private Sub btnBox8_Click(sender As Object, e As EventArgs) Handles btnBox8.Click
+        If blnXO = True Then
+            btnBox8.Text = "X"
+            blnXO = False
+        Else
+            btnBox8.Text = "O"
+            blnXO = True
+        End If
+        btnBox8.Enabled = False
+        checkForWinner()
+    End Sub
+
+    Private Sub btnBox9_Click(sender As Object, e As EventArgs) Handles btnBox9.Click
+        If blnXO = True Then
+            btnBox9.Text = "X"
+            blnXO = False
+        Else
+            btnBox9.Text = "O"
+            blnXO = True
+        End If
+        btnBox9.Enabled = False
+        checkForWinner()
+    End Sub
+
+    Private Sub checkForWinner()
+        'Horizontals checks
+        If btnBox1.Text <> Nothing And btnBox1.Text = btnBox2.Text And btnBox2.Text = btnBox3.Text Then
+            buttonDisable()
+            btnBox1.BackColor = Color.Pink
+            btnBox2.BackColor = Color.Pink
+            btnBox3.BackColor = Color.Pink
+            MessageBox.Show(btnBox1.Text + " is the winner!")
+        ElseIf btnBox4.Text <> Nothing And btnBox4.Text = btnBox5.Text And btnBox5.Text = btnBox6.Text Then
+            buttonDisable()
+            btnBox4.BackColor = Color.Pink
+            btnBox5.BackColor = Color.Pink
+            btnBox6.BackColor = Color.Pink
+            MessageBox.Show(btnBox4.Text + " is the winner!")
+        ElseIf btnBox7.Text <> Nothing And btnBox7.Text = btnBox8.Text And btnBox8.Text = btnBox9.Text Then
+            buttonDisable()
+            btnBox7.BackColor = Color.Pink
+            btnBox8.BackColor = Color.Pink
+            btnBox9.BackColor = Color.Pink
+            MessageBox.Show(btnBox7.Text + " is the winner!")
+        End If
+
+        'Verticals checks
+        If btnBox1.Text <> Nothing And btnBox1.Text = btnBox4.Text And btnBox4.Text = btnBox7.Text Then
+            buttonDisable()
+            btnBox1.BackColor = Color.Pink
+            btnBox4.BackColor = Color.Pink
+            btnBox7.BackColor = Color.Pink
+            MessageBox.Show(btnBox1.Text + " is the winner!")
+        ElseIf btnBox2.Text <> Nothing And btnBox2.Text = btnBox5.Text And btnBox5.Text = btnBox8.Text Then
+            buttonDisable()
+            btnBox2.BackColor = Color.Pink
+            btnBox5.BackColor = Color.Pink
+            btnBox8.BackColor = Color.Pink
+            MessageBox.Show(btnBox2.Text + " is the winner!")
+        ElseIf btnBox3.Text <> Nothing And btnBox3.Text = btnBox6.Text And btnBox6.Text = btnBox9.Text Then
+            buttonDisable()
+            btnBox3.BackColor = Color.Pink
+            btnBox6.BackColor = Color.Pink
+            btnBox9.BackColor = Color.Pink
+            MessageBox.Show(btnBox3.Text + " is the winner!")
+        End If
+
+        'Diagonal checks
+        If btnBox1.Text <> Nothing And btnBox1.Text = btnBox5.Text And btnBox5.Text = btnBox9.Text Then
+            buttonDisable()
+            btnBox1.BackColor = Color.Pink
+            btnBox5.BackColor = Color.Pink
+            btnBox9.BackColor = Color.Pink
+            MessageBox.Show(btnBox1.Text + " is the winner!")
+        ElseIf btnBox3.Text <> Nothing And btnBox3.Text = btnBox5.Text And btnBox5.Text = btnBox7.Text Then
+            buttonDisable()
+            btnBox3.BackColor = Color.Pink
+            btnBox5.BackColor = Color.Pink
+            btnBox7.BackColor = Color.Pink
+            MessageBox.Show(btnBox3.Text + " is the winner!")
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        blnXO = True
+        btnBox1.Text = Nothing
+        btnBox2.Text = Nothing
+        btnBox3.Text = Nothing
+        btnBox4.Text = Nothing
+        btnBox5.Text = Nothing
+        btnBox6.Text = Nothing
+        btnBox7.Text = Nothing
+        btnBox8.Text = Nothing
+        btnBox9.Text = Nothing
+        btnBox1.Enabled = True
+        btnBox2.Enabled = True
+        btnBox3.Enabled = True
+        btnBox4.Enabled = True
+        btnBox5.Enabled = True
+        btnBox6.Enabled = True
+        btnBox7.Enabled = True
+        btnBox8.Enabled = True
+        btnBox9.Enabled = True
+        btnBox1.BackColor = DefaultBackColor
+        btnBox2.BackColor = DefaultBackColor
+        btnBox3.BackColor = DefaultBackColor
+        btnBox4.BackColor = DefaultBackColor
+        btnBox5.BackColor = DefaultBackColor
+        btnBox6.BackColor = DefaultBackColor
+        btnBox7.BackColor = DefaultBackColor
+        btnBox8.BackColor = DefaultBackColor
+        btnBox9.BackColor = DefaultBackColor
+    End Sub
+    Private Sub buttonDisable()
+        btnBox1.Enabled = False
+        btnBox2.Enabled = False
+        btnBox3.Enabled = False
+        btnBox4.Enabled = False
+        btnBox5.Enabled = False
+        btnBox6.Enabled = False
+        btnBox7.Enabled = False
+        btnBox8.Enabled = False
+        btnBox9.Enabled = False
+    End Sub
+
+End Class
